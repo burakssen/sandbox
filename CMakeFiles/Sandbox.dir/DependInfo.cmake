1660b3872fc1c86ecc6e79b8238dcc55fc6a2455
burakssen <buraksen7@hotmail.com>
2025-08-26 19:45:47 +0000

    Deploying to gh-pages from  @ 7a1a8298f919d802230019f1b8cd24919aec4f76 🚀

---
+++ CMakeFiles/Sandbox.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/runner/work/sandbox/sandbox/src/raygui.c" "CMakeFiles/Sandbox.dir/src/raygui.c.o" "gcc" "CMakeFiles/Sandbox.dir/src/raygui.c.o.d"
+  "/home/runner/work/sandbox/sandbox/src/core/Application.cpp" "CMakeFiles/Sandbox.dir/src/core/Application.cpp.o" "gcc" "CMakeFiles/Sandbox.dir/src/core/Application.cpp.o.d"
+  "/home/runner/work/sandbox/sandbox/src/core/PixelWorld.cpp" "CMakeFiles/Sandbox.dir/src/core/PixelWorld.cpp.o" "gcc" "CMakeFiles/Sandbox.dir/src/core/PixelWorld.cpp.o.d"
+  "/home/runner/work/sandbox/sandbox/src/core/Renderer.cpp" "CMakeFiles/Sandbox.dir/src/core/Renderer.cpp.o" "gcc" "CMakeFiles/Sandbox.dir/src/core/Renderer.cpp.o.d"
+  "/home/runner/work/sandbox/sandbox/src/main.cpp" "CMakeFiles/Sandbox.dir/src/main.cpp.o" "gcc" "CMakeFiles/Sandbox.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/Sandbox.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sandbox.dir/src/core/Application.cpp.o"
+  "CMakeFiles/Sandbox.dir/src/core/Application.cpp.o.d"
+  "CMakeFiles/Sandbox.dir/src/core/PixelWorld.cpp.o"
+  "CMakeFiles/Sandbox.dir/src/core/PixelWorld.cpp.o.d"
+  "CMakeFiles/Sandbox.dir/src/core/Renderer.cpp.o"
+  "CMakeFiles/Sandbox.dir/src/core/Renderer.cpp.o.d"
+  "CMakeFiles/Sandbox.dir/src/main.cpp.o"
+  "CMakeFiles/Sandbox.dir/src/main.cpp.o.d"
+  "CMakeFiles/Sandbox.dir/src/raygui.c.o"
+  "CMakeFiles/Sandbox.dir/src/raygui.c.o.d"
+  "Sandbox.js"
+  "Sandbox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/Sandbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
